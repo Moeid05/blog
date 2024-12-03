f82005807c28a60ee7f514c0112accc0d41b2f77
Moeid <ostadsisq@gmail.com>
2024-12-03 22:15:48 +0330

    blog model has been defined

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    publish_date = models.DateTimeField(auto_now_add=True)  # Automatically set the field to now when the object is first created
+    updated_at = models.DateTimeField(auto_now=True)  # Automatically update the field to now every time the object is saved
+
+    class Meta:
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+        ordering = ['-publish_date']
+
+    def __str__(self):
+        return self.title    
