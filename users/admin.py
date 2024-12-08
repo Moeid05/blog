9cffcbd5ca802c45a498530906c83dcdc52ca8ae
Moeid <ostadsisq@gmail.com>
2024-12-08 19:03:16 +0330

    naming was cus of bug

--- users/admin.py
+++ users/admin.py
@@ -2,15 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser 
 
-class CustomUser Admin(UserAdmin):
-    # Specify the fields to be displayed in the admin list view
+class CustomUser_Admin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'total_vote', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_active', 'groups')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     ordering = ('username',)
     filter_horizontal = ('groups', 'user_permissions', 'followers')
-
-    # Specify which fields are editable in the admin form
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('total_vote',)}),
     )
@@ -19,5 +16,4 @@ class CustomUser Admin(UserAdmin):
         (None, {'fields': ('total_vote',)}),
     )
 
-# Register the CustomUser  model with the CustomUser Admin
-admin.site.register(CustomUser , CustomUser Admin)
+admin.site.register(CustomUser , CustomUser_Admin)
